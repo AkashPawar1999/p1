f058ca58ceef0d63809b2176b5299370cb77e6a6
akash <akash123@gmail.com>
2024-03-21 12:26:54 +0530

    fixed login and signup working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,16 +15,17 @@ export class LoginComponent implements OnInit{
   constructor(private http: HttpClient, private router: Router) { }
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      'firstName': new FormControl(null),
+      // 'firstName': new FormControl(null),
       'email': new FormControl(null),
       'password': new FormControl(null),
     })
   }
 
   onSave() {
-    console.log(this.loginForm.value);
+    // console.log(this.loginForm.value);
     this.http.get("http://localhost:3000/posts").subscribe(res => {
-        const user = res.isPrototypeOf((a: any) => {
+      const userArray = Object.values(res);
+        const user = userArray.find((a: any) => {
           return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password;
         })
         if(user){
@@ -33,7 +34,7 @@ export class LoginComponent implements OnInit{
           this.router.navigate(['dashboard']);
         }
         else{
-          alert('User not Found !')
+          alert('User not Found, something went wrong!')
           this.loginForm.reset();
         }
     },err => {
